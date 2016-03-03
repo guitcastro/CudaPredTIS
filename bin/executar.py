cc4f6476570fa41b6b37f52ce2fc71c216adcacd
Guilherme Torres Castro <guilherme.torres.castro@gmail.com>
2016-03-03 10:20:56 -0300

    Update executar.py

--- bin/executar.py
+++ bin/executar.py
@@ -27,37 +27,36 @@ postiveCount = fileHandle.createFinalTestsFiles(workingDir,folds)
 
 fileHandle.createClustersFiles(workingDir,folds)
 
-#for i in range (1,folds+1):
-#    commandLine = '(time ' + './mpi-means ' + \
-#                workingDir + 'cluster' + str(i) + \
-#                ' ' + str(totalPositive - postiveCount[i-1]) + \
-#                ') &> ' + workingDir +  'log' + str(i) + '.txt'
-#    print commandLine
-#    os.system(commandLine)
-#
-#fileHandle.createTrainFiles(workingDir,folds)
-#
-#svm_learn = './svm_learn'
-#svm_classify = './svm_classify'
-#if platform.system() == 'Darwin':
-#    svm_learn = './svm_learn_osx'
-#    svm_classify = './svm_classify_osx'
-#
-#for i in range (1,folds+1):
-#    commandLine = svm_learn + ' -t 1 -d 4 -c 0.5 ' + \
-#               workingDir + 'train' + str(i) + \
-#               ' ' + workingDir + 'model' + str(i)
-#    print commandLine
-#    os.system(commandLine)
-#
-#for i in range (1,folds+1):
-#    commandLine = svm_classify  + \
-#               ' ' + workingDir + 'test' + str(i) + \
-#               ' ' + workingDir + 'model' + str(i) + \
-#               ' ' + workingDir + 'predictions' + str(i)
-#    print commandLine
-#    os.system(commandLine)
-#
-#
-#resultHandle.evalResults(workingDir,folds)
-#
+for i in range (1,folds+1):
+    commandLine = '(time ' + './mpi-means ' + \
+                workingDir + 'cluster' + str(i) + \
+                ' ' + str(totalPositive - postiveCount[i-1]) + \
+                ') &> ' + workingDir +  'log' + str(i) + '.txt'
+    print commandLine
+    os.system(commandLine)
+
+fileHandle.createTrainFiles(workingDir,folds)
+
+svm_learn = './svm_learn'
+svm_classify = './svm_classify'
+if platform.system() == 'Darwin':
+    svm_learn = './svm_learn_osx'
+    svm_classify = './svm_classify_osx'
+
+for i in range (1,folds+1):
+    commandLine = svm_learn + ' -t 1 -d 4 -c 0.5 ' + \
+               workingDir + 'train' + str(i) + \
+               ' ' + workingDir + 'model' + str(i)
+    print commandLine
+    os.system(commandLine)
+
+for i in range (1,folds+1):
+    commandLine = svm_classify  + \
+               ' ' + workingDir + 'test' + str(i) + \
+               ' ' + workingDir + 'model' + str(i) + \
+               ' ' + workingDir + 'predictions' + str(i)
+    print commandLine
+    os.system(commandLine)
+
+resultHandle.evalResults(workingDir,folds)
+
